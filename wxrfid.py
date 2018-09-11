4250c9f5c4430afb188c832bf6227f3f8d2eb40a
nlderek <derekwaikl@gmail.com>
2018-09-11 12:43:39 +0800

    Update on 20180911 1245

--- wxrfid.py
+++ wxrfid.py
@@ -1,52 +1,116 @@
 ''' Modules required: wxPython '''
 
 import wx
+import wx.grid
 import os
+import pandas as pd
+import numpy as np
 
  
-class MDIFrame(wx.MDIParentFrame): 
+class MDIFrame(wx.MDIParentFrame):
     def __init__(self,*args, **kwargs):
         wx.MDIParentFrame.__init__(self, None, -1, "Winnington RFID ")#, size = (600,400))
         self.InitUI()
     # Set Menubar on parent frame
-        
-    def InitUI(self):
-        
+    def InitUI(self):  
         menubar = wx.MenuBar() 
-        menu = wx.Menu()
-        qmi = wx.MenuItem(menu, ID_DECODE, '&Decoder')
-        qmi.SetBitmap(wx.Bitmap(os.getcwd() + "\icons\decode.ico")) 
-        qmi = wx.MenuItem(menu, ID_ENCODE, '&Decoder')
-        qmi.SetBitmap(wx.Bitmap(os.getcwd() + "\icons\encode.ico"))
-        qmi = wx.MenuItem(menu, ID_EXIT, '&Decoder')
-        qmi.SetBitmap(wx.Bitmap(os.getcwd() + "\icons\wtonlogo.ico"))        
-        #menu.Append(5000, "&Decoder") 
-        #menu.Append(5001, "&Encoder")
-        #menu.Append(5002, "&Exit")
-
-        self.Bind(wx.EVT_MENU, self.Decoder, id = ID_DECODE) 
-        self.Bind(wx.EVT_MENU, self.Encoder, id = ID_ENCODE)
-        self.Bind(wx.EVT_MENU, self.OnExit, id = ID_EXIT)
-        
-        menubar.Append(qmi, "&Options") 
-		self.SetMenuBar(menubar)
-
-    # Set icon Menubar
-        
-    def OnExit(self, evt): 
+        optionmenu = wx.Menu()
+        encodeMenuitem = optionmenu.Append(wx.NewId(),"Encode")
+        self.Bind(wx.EVT_MENU, self.Encoder, encodeMenuitem)
+        decodeMenuitem = optionmenu.Append(wx.NewId(),"Decode")
+        self.Bind(wx.EVT_MENU, self.Decoder, decodeMenuitem)
+        exitMenuitem = optionmenu.Append(wx.NewId(),"Exit")
+        menubar.Append(optionmenu,"&Options")
+        self.Bind(wx.EVT_MENU, self.OnExit, exitMenuitem)
+        self.SetMenuBar(menubar)
+       
+    def OnExit(self, event):
         self.Close(True)  
 		
-    def Decoder(self, evt): 
-        win = wx.MDIChildFrame(self, -1,  "Decoder Window",size = (800,600) )
+    def Decoder(self, event): 
+        #win = wx.MDIChildFrame(self, -1,  "Decoder Window",size = (1024,600) )
+        win = DECODEFrame(self)
         win.Show(True)
         win.SetIcon(wx.Icon(os.getcwd() + "\icons\decode.ico"))
-        
-    
-    def Encoder(self, evt): 
+            
+    def Encoder(self, event): 
         win = wx.MDIChildFrame(self, -1, "Encoder Window",size = (800,600) )
         win.Show(True)
         win.SetIcon(wx.Icon(os.getcwd() + "\icons\encode.ico"))
-		
+
+class DataTable(wx.grid.GridTableBase):
+    EVEN_ROW_COLOUR = '#CCE6FF'
+    GRID_LINE_COLOUR = '#ccc'
+
+    def __init__(self, data=None):
+        wx.grid.GridTableBase.__init__(self)
+        self.headerRows = 1
+        if data is None:
+            data = pd.DataFrame()
+        self.data = data
+
+    def GetNumberRows(self):
+        return len(self.data)
+    
+    def GetNumberCols(self):
+        return len(self.data.columns) + 1
+    
+    def GetValue(self, row, col):
+        if col == 0:
+            return self.data.index[row]
+        return self.data.iloc[row, col-1]
+    
+    def SetValue(self, row, col, value):
+        self.data.iloc[row,col -1] = value
+    
+    def GetColLabelValue(self, col):
+        if col == 0:
+            if self.data.index.name is None:
+                return 'Index'
+            else:
+                return self.data.index.name
+        return str(self.data.columns[col-1])
+    
+    def GetTypeName(self, row, col):
+        return wx.grid.GRID_VALUE_STRING
+
+    def GetAttr(self, row, col, prop):
+        attr = wx.grid.GridCellAttr()
+        if row % 2 == 1:
+            attr.SetBackgroundColour(EVEN_ROW_COLOUR)
+        return attr
+
+class DECODEFrame(wx.MDIChildFrame): 
+    def __init__(self,parent):
+        wx.MDIChildFrame.__init__(self, parent, title= "Decoder Window", size = (1024,600))
+        panel = wx.Panel(self,-1)
+        self._init_gui()
+
+    def _init_gui(self):
+        df = pd.DataFrame(np.random.random((10,5)))
+        table = DataTable(df)
+
+        grid = wx.grid.Grid(self,-1)
+        grid.SetTable(table,takeOwnership=True)
+        grid.AutoSizeColumns()
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(grid,1,wx.EXPAND)
+        self.SetSizer(sizer)
+
+        self.Bind(wx.EVT_CLOSE, self.quit)
+
+    def quit(self,event):
+        self.Destroy()
+#class DataGrid(gridlib.Grid):
+#    def __init__(self, parent, size=wx.Size(1000, 500)):
+#        self.parent = parent
+#        gridlib.Grid.__init__(self, self.parent, -1)
+#        self.SetGridLineColour(GRID_LINE_COLOUR)
+#        self.SetRowLabelSize(0)
+#        self.SetColLabelSize(30)
+#        self.table = DataTable()
+
 app = wx.App() 
 frame = MDIFrame() 
 frame.SetIcon(wx.Icon(os.getcwd() + "\icons\wtonlogo.ico"))
